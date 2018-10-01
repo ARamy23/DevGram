19c31bada229180f04dd892efdebd34817b58a01
Ahmed Ramy <Dev.AhmedRamy@iCloud.com>
2018-10-01 15:44:33 +0200

    added and fixed displaying the tabBar icons to be centered

--- Devgram/AppComponents/MainTabBarController.swift
+++ Devgram/AppComponents/MainTabBarController.swift
@@ -26,23 +26,38 @@ class MainTabBarController: UITabBarController {
     
     //MARK:- Setup Methods
     
-    fileprivate func setupTabBarUI()
+    fileprivate func setupTabBar()
     {
         view.backgroundColor = .white
         tabBar.tintColor = .appPrimaryColor
+        delegate = self
     }
     
     fileprivate func setupVCs()
     {
         viewControllers = [
+            generateNavCon(viewController: UIViewController(), name: "Home", image: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected")),
+            generateNavCon(viewController: UIViewController(), name: "Serach", image: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected")),
+            generateNavCon(viewController: UIViewController(), name: "Plus", image: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected")),
+            generateNavCon(viewController: UIViewController(), name: "Like", image: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected")),
             generateNavCon(viewController: ProfileVC(collectionViewLayout: UICollectionViewFlowLayout()), name: "My Profile", image: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"))
         ]
     }
     
     func setupUI()
     {
-        setupTabBarUI()
+        setupTabBar()
         setupVCs()
+        setupTabBarIconsCorrectly()
+    }
+    
+    fileprivate func setupTabBarIconsCorrectly()
+    {
+        guard let tabBarItems = tabBar.items else { return }
+        for item in tabBarItems
+        {
+            item.imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
+        }
     }
     
     //MARK:- Helper Methods
@@ -53,7 +68,7 @@ class MainTabBarController: UITabBarController {
         navVC.navigationBar.topItem?.title = title
         navVC.tabBarItem.title = title
         navVC.tabBarItem.image = image
-        navVC.tabBarItem.selectedImage = selectedImage ?? image
+        navVC.tabBarItem.selectedImage = selectedImage
         navVC.navigationBar.tintColor = #colorLiteral(red: 0.4235294118, green: 0.4274509804, blue: 0.7921568627, alpha: 1)
         return navVC
     }
@@ -66,5 +81,14 @@ class MainTabBarController: UITabBarController {
         {
             present(generateNavCon(viewController: LoginVC()), animated: true, completion: nil)
         }
+    }
+}
+
+extension MainTabBarController: UITabBarControllerDelegate
+{
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        let index = viewControllers?.index(of: viewController)
+        
+        return !(index == 2)
     }
 }
